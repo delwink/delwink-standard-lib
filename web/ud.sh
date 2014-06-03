1dc3445053c79aac45d112877a360fb89d6d52a3
David McMackins <mcmackinsd@gmail.com>
2014-06-02 20:54:03 -0500

    Made more generic

--- web/ud.sh
+++ web/ud.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+LOGINUSER=XXX
+LOGINHOST=XXX.XXX.XXX.XXX
+WEBROOT=/srv/http
+
 # Converts all markdown to hypertext markup
 
 for OUTPUT in $(find . -type f -name '*.md' -exec echo {} \;)
@@ -7,6 +11,6 @@ do
     ./convert.sh ${OUTPUT%.md}
 done
 
-# Copies all the things to mcmackins.guru's web directory
+# Copies all the things to remote server's web directory
 
-scp -r * mac@guru:/srv/http
+scp -r * $LOGINUSER@$LOGINHOST:$WEBROOT
