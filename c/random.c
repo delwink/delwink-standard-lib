b4df593c86cf4f854b0853785d03a736773030eb
David McMackins <mcmackinsd@gmail.com>
2014-06-07 13:21:21 -0500

    Added get random function

---
+++ c/random.c
@@ -0,0 +1,9 @@
+#include "random.h"
+
+/**
+ * Returns a random integer between two values.
+ */
+int get_random(int top, int bottom)
+{
+    return bottom + (rand() % top - bottom) + 1;
+}
---
+++ c/random.h
@@ -0,0 +1,6 @@
+#ifndef _DELWINK_RANDOM_H
+#define _DELWINK_RANDOM_H
+
+int get_random(int,int);
+
+#endif
