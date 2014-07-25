167642d629eed1a3a5d883824a31086534f61def
David McMackins <mcmackinsd@gmail.com>
2014-07-25 15:46:33 -0500

    Added conversion from byte to binary string

---
+++ c/strbin.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+#include "strbin.h"
+
+const char *byte_to_binary(int x)
+{
+    static char b[9];
+    b[0] = '\0';
+
+    int z;
+    for (z = 128; z > 0; z >>= 1)
+    {
+        strcat(b, ((x & z) == z) ? "1" : "0");
+    }
+
+    return b;
+}
---
+++ c/strbin.h
@@ -0,0 +1,6 @@
+#ifndef _DELWINK_STRBIN_H
+#define _DELWINK_STRBIN_H
+
+const char *byte_to_binary(int);
+
+#endif
