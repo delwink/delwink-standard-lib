0a6c49e60966e36e677438d7fcbed325b3cb73af
David McMackins <mcmackinsd@gmail.com>
2014-07-26 20:44:56 -0500

    Removed web

--- web/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Takes an argument and converts a markdown file to a mcmackins.guru HTML file
-
-cat HEADER > $1.html
-markdown MDHEADER.md >> $1.html
-markdown $1.md >> $1.html
-cat FOOTER >> $1.html
