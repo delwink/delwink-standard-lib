857c5c7672f8aca01466db00e781e6303c8bccfa
David McMackins <mcmackinsd@gmail.com>
2014-08-09 18:30:50 -0500

    Update logging

--- c/log.c
+++ c/log.c
@@ -1,5 +1,7 @@
+#include <string.h>
 #include <time.h>
 #include "log.h"
+#include "datestamp.h"
 
 const FILE *LOG_FILE;
 const uint8_t LOGGING;
@@ -18,5 +20,11 @@ void log_init(char *f)
 
 char *get_name()
 {
-    
+    const char *ext = ".log";
+    const int extlen = strlen(ext);
+    int prelen = strlen(PROG);
+    char *s = malloc(prelen+extlen+15);
+    strcat(s, PROG);
+    strcat(s, delwink_get_date_stamp());
+    strcat(s, ext);
 }
