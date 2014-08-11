ee51afcff3a7ad92efaf450d105ef216ee91ed71
David McMackins <contact@mcmackins.org>
2014-08-11 18:52:29 -0500

    Added errno to logging

--- c/log.c
+++ c/log.c
@@ -1,30 +1,34 @@
-#include <string.h>
 #include <time.h>
 #include "log.h"
 #include "datestamp.h"
 
-const FILE *LOG_FILE;
+FILE *LOG_FILE;
 const uint8_t LOGGING;
 
-char *get_name();
+char *get_log_file_name(const char *);
 
-void log_init(char *f)
+char *log_init(const char *f)
 {
-    LOG_FILE = fopen(get_name(), "w");
+    char *logfilename = get_log_file_name(f);
+    LOG_FILE = fopen(logfilename, "w");
     if (LOG_FILE == NULL) {
         LOGGING = 0;
     } else {
         LOGGING = 1;
+        return logfilename;
     }
+    return NULL;
 }
 
-char *get_name()
+char *get_log_file_name(const char *loc)
 {
     const char *ext = ".log";
     const int extlen = strlen(ext);
-    int prelen = strlen(PROG);
+    int prelen = strlen(loc);
     char *s = malloc(prelen+extlen+15);
-    strcat(s, PROG);
+    strcat(s, loc);
     strcat(s, delwink_get_date_stamp());
     strcat(s, ext);
+
+    return s;
 }
--- c/log.h
+++ c/log.h
@@ -3,16 +3,17 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <errno.h>
+#include <string.h>
 
-#define PROG "FIXME"
-
-extern const FILE *LOG_FILE;
+extern FILE *LOG_FILE;
 extern const uint8_t LOGGING;
 
 /**
- * Initializes logging to `file`
+ * Initializes logging to a file in `path`
+ * Returns full path to log file
  */
-void log_init(char *file);
+char *log_init(char *path);
 
 #define log_info(M, ...) {\
     fprintf(stdout, "[INFO] " M "\n", ##__VA_ARGS__);\
@@ -20,13 +21,22 @@ void log_init(char *file);
 }
 
 #define log_warn(M, ...) {\
-    fprintf(stdout, "[WARNING] " M "\n", ##__VA_ARGS__);\
-    if (LOGGING) fprintf(LOG_FILE, "[WARNING] " M "\n", ##__VA_ARGS__);\
-}
+    if (errno) {\
+        fprintf(stderr, "[WARNING] " M ": %s\n", ##__VA_ARGS__, strerror(errno));\
+        if (LOGGING) fprintf(LOG_FILE, "[WARNING] " M ": %s\n", ##__VA_ARGS__, strerror(errno));\
+    } else {\
+        fprintf(stderr, "[WARNING] " M "\n", ##__VA_ARGS__);\
+        if (LOGGING) fprintf(LOG_FILE, "[WARNING] " M "\n", ##__VA_ARGS__);\
+    }\}
 
 #define log_err(M, ...) {\
-    fprintf(stderr, "[FATAL] " M "\n", ##__VA_ARGS__);\
-    if (LOGGING) fprintf(LOG_FILE, "[FATAL] " M "\n", ##__VA_ARGS__);\
+    if (errno) {\
+        fprintf(stderr, "[FATAL] " M ": %s\n", ##__VA_ARGS__, strerror(errno));\
+        if (LOGGING) fprintf(LOG_FILE, "[FATAL] " M ": %s\n", ##__VA_ARGS__, strerror(errno));\
+    } else {\
+        fprintf(stderr, "[FATAL] " M "\n", ##__VA_ARGS__);\
+        if (LOGGING) fprintf(LOG_FILE, "[FATAL] " M "\n", ##__VA_ARGS__);\
+    }\
 }
 
 #endif
